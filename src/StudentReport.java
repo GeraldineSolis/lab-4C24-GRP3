a7a623f1d3451b2a46db30e4e97cbfe4519ffb74
GeraldineSolis <solis.geraldine@tecsup.edu.pe>
2025-09-01 12:02:27 -0500

    Geraldine: agregando cambios en mi clase

--- src/StudentReport.java
+++ src/StudentReport.java
@@ -1,7 +1,8 @@
 package src;
 // Colaborador 2
+//MODIFICADO MODERADOR
 public class StudentReport {
     public static void main(String[] args) {
-        System.out.println("Hola, bienvenidos!");
+        System.out.println("Hola, bienvenidos a todos los presentes!");
     }
 }
